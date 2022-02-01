9293de066de3521d795ee87a88fbf4853d7cf965
Meron Seyoum <meryato@gmail.com>
2022-02-01 12:18:42 -0700

    Note servlet is created with both do and get function read and write to be add later

--- src/main/java/ca/sait/week4lab_simplenotekeeper/servlets/NoteServlet.java
+++ src/main/java/ca/sait/week4lab_simplenotekeeper/servlets/NoteServlet.java
@@ -17,32 +17,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class NoteServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NoteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NoteServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -55,7 +29,14 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String operator = request.getParameter("operator");
+        request.setAttribute("note", readNote());
+        if (operator != null && operator.equals("operator")) {
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+
+        }
     }
 
     /**
@@ -69,17 +50,24 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+//variables
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+
+        if (title == null || title.equals("") || content == null || content.equals("")) {
+            request.setAttribute("title", title);
+            request.setAttribute("content", content);
+            request.setAttribute("invalid", true);
+
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+        } else {
+           
+            writeNote(title, content);
+            request.setAttribute("note", readNote());
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+   
 }
